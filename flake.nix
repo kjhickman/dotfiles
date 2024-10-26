24ea23fb49687edfa44331b100eecdba1f69ab0e
Kyle Hickman <kyle@txhickman.com>
2024-10-26 18:31:24 -0500

    fix darwin path and wsl config

--- flake.nix
+++ flake.nix
@@ -1,18 +1,20 @@
 {
-  description = "Example Darwin system flake";
+  description = "Kyle's flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nixos-wsl = { url = "github:nix-community/nixos-wsl"; };
 
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }: {
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager, nixos-wsl }: {
     darwinConfigurations."macos" = nix-darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       specialArgs = { inherit inputs; };
@@ -35,6 +37,35 @@
               home = {
                 username = "kyle";
                 homeDirectory = "/Users/kyle";
+              };
+            };
+          };
+        }
+      ];
+    };
+
+    nixosConfigurations."wsl" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./wsl
+        nixos-wsl.nixosModules.wsl
+        home-manager.nixosModules.home-manager
+        {
+          users.users.kyle = {
+            name = "kyle";
+            home = "/home/kyle";
+          };
+
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = { inherit inputs; };
+            users.kyle = { config, pkgs, ... }: {
+              imports = [ ./home ];
+              home = {
+                username = "kyle";
+                homeDirectory = "/home/kyle";
               };
             };
           };
