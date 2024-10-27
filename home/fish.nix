0b8840800463e177d9da5e0cd3cddde30d5ba813
Kyle Hickman <kyle@txhickman.com>
2024-10-27 18:09:06 -0500

    disable fish greeting

--- home/fish.nix
+++ home/fish.nix
@@ -5,10 +5,10 @@
     enable = true;
 
     interactiveShellInit = ''
-      function starship_transient_prompt_func
-        starship module character
-      end
-      starship init fish | source
+      set fish_greeting # Disable greeting
+    '';
+    
+    shellInitLast = ''
       enable_transience
     '';
   };
