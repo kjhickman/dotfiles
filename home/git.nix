5b235cc829a4f8046fc79b91c26abcd034774bd4
Kyle Hickman <kyle@txhickman.com>
2025-03-18 00:32:20 -0500

    add more git settings

--- home/git.nix
+++ home/git.nix
@@ -6,12 +6,16 @@
     userName = "Kyle Hickman";
     userEmail = "kyle@txhickman.com";
     extraConfig = {
-      init.defaultBranch = "main";
+      branch.sort = "-committerdate";
+      column.ui = "auto";
+      commit.verbose = true;
       core.editor = "nvim";
       core.autocrlf = "input";
-      push.default = "current";
+      core.excludesfile = "~/.gitignore";
+      help.autocorrect = "prompt";
+      init.defaultBranch = "main";
+      tag.sort = "version:refname";
 
-      # Git Aliases
       alias.co = "switch";
       alias.up = "!git pull --rebase --prune $@ && git submodule update --init --recursive";
       alias.cob = "switch -c";
@@ -19,6 +23,36 @@
       alias.save = "!git add -A && git commit -m 'wip'";
       alias.undo = "reset HEAD~1 --mixed";
       alias.diffshow =  "!f() { git diff \$1 \${2-HEAD} --name-status ; }; f";
+
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
     };
   };
 }
