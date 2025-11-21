20212ded0ee56c2a54b17ae1b6c3e821356c6804
Kyle Hickman <kyle@txhickman.com>
2025-11-21 14:05:06 -0600

    use homebrew for dotnet

--- home/dotnet.nix
+++ home/dotnet.nix
@@ -1,11 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    (with dotnetCorePackages; combinePackages [
-      sdk_8_0
-      sdk_9_0
-      sdk_10_0
-    ])
-  ];
+  # .NET SDK installed via Homebrew to avoid macOS code signing issues
+  # See homebrew.nix for dotnet-sdk@8, dotnet-sdk@9, and dotnet-sdk (latest)
+  home.packages = [ ];
 }
