658782734f41fb48323ba755f759e461007f67e0
Kyle Hickman <kyle@txhickman.com>
2026-02-03 03:03:52 +0000

    add superpowers to opencode config

--- home/modules/opencode.nix
+++ home/modules/opencode.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}: 
+{ config, lib, pkgs, ... }:
 
 {
   programs.opencode = {
@@ -9,4 +9,23 @@
   };
 
   xdg.configFile."opencode/opencode.json".force = true;
+
+  home.activation.installSuperpowers = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    superpowers_dir="${config.home.homeDirectory}/.config/opencode/superpowers"
+    superpowers_parent="${config.home.homeDirectory}/.config/opencode"
+
+    if [ ! -d "$superpowers_dir/.git" ]; then
+      mkdir -p "$superpowers_parent"
+      ${pkgs.git}/bin/git clone https://github.com/obra/superpowers.git "$superpowers_dir"
+    else
+      ${pkgs.git}/bin/git -C "$superpowers_dir" fetch --all --prune
+      ${pkgs.git}/bin/git -C "$superpowers_dir" pull --ff-only
+    fi
+  '';
+
+  home.file."${config.home.homeDirectory}/.config/opencode/plugins/superpowers.js".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/opencode/superpowers/.opencode/plugins/superpowers.js";
+
+  home.file."${config.home.homeDirectory}/.config/opencode/skills/superpowers".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/opencode/superpowers/skills";
 }
