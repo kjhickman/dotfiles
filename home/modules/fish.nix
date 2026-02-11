424b22c76f5f920645b443289927d45d5598079e
Kyle Hickman <kyle@txhickman.com>
2026-02-11 15:41:35 -0600

    add transient prompt

--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -12,17 +12,25 @@ in
 
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
+      set -g fish_transient_prompt 1
     '';
 
     functions = {
       fish_prompt = ''
+        set -l last_status $status
+
+        if contains -- --final-rendering $argv
+          prmt --shell none --no-version --code $last_status '{path:cyan:s} {ok:green}{fail:red} '
+          return
+        end
+
         if set -q __prmt_prompt_seen
           echo
         else
           set -g __prmt_prompt_seen 1
         end
 
-        prmt --shell none --no-version --code $status '{path:cyan} {git:purple}\n{ok:green}{fail:red} '
+        prmt --shell none --no-version --code $last_status '{path:cyan} {git:purple}\n{ok:green}{fail:red} '
       '';
     };
   };
